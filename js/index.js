19e289f1b336337e06cb1a9cc184b3836b6c21f5
Marcellah021 <marcellahernandes4@gmail.com>
2026-06-02 16:49:39 -0400

    feat: adiciona as garantias no site

--- js/index.js
+++ js/index.js
@@ -1,31 +1,107 @@
 const video = document.getElementById('video');
+const whatsapp = document.querySelector('.whatsapp-float');
 
+function bloquearNavegacao(e) {
+  e.preventDefault();
+}
 
-video.addEventListener('click', () => {
+function bloquearTeclas(e) {
+  const teclasBloqueadas = [
+    'ArrowUp',
+    'ArrowDown',
+    'PageUp',
+    'PageDown',
+    'Home',
+    'End',
+    ' ',
+    'Spacebar'
+  ];
+
+  if (teclasBloqueadas.includes(e.key)) {
+    e.preventDefault();
+  }
+}
+
+function ativarBloqueio() {
+  document.body.style.overflow = 'hidden';
+
+  window.addEventListener('wheel', bloquearNavegacao, {
+    passive: false
+  });
+
+  window.addEventListener('touchmove', bloquearNavegacao, {
+    passive: false
+  });
+
+  window.addEventListener('keydown', bloquearTeclas);
+}
+
+function desativarBloqueio() {
+  document.body.style.overflow = 'auto';
+
+  window.removeEventListener('wheel', bloquearNavegacao);
+
+  window.removeEventListener('touchmove', bloquearNavegacao);
+
+  window.removeEventListener('keydown', bloquearTeclas);
+}
+
+if (video) {
+
+  // Bloqueia navegação
+  ativarBloqueio();
+
+  // Play / Pause ao clicar
+  video.addEventListener('click', () => {
     if (video.paused) {
-        video.play();
+      video.play();
     } else {
-        video.pause();
+      video.pause();
     }
-});
+  });
+
+  // Quando terminar o vídeo
+  video.addEventListener('ended', () => {
+
+    desativarBloqueio();
+    // Vai para próxima seção
+    const certificacoes = document.querySelector('.certificacoes');
+
+    if (certificacoes) {
+      certificacoes.scrollIntoView({
+        behavior: 'smooth'
+      });
+    }
+  });
+}
+
 
 const buttons = document.querySelectorAll('.btnSM');
 
 buttons.forEach(button => {
   button.addEventListener('click', () => {
-    const text = button.previousElementSibling; // pega o <p> antes do botão
-    text.classList.toggle('show'); // ativa/desativa a animação
+
+    const text = button.previousElementSibling;
+
+    text.classList.toggle('show');
+
+    if (text.classList.contains('show')) {
+      button.textContent = 'Mostrar menos';
+    } else {
+      button.textContent = 'Saiba mais';
+    }
   });
 });
 
-document.addEventListener("DOMContentLoaded", () => {
-    const whatsapp = document.querySelector(".whatsapp-float");
+document.addEventListener('DOMContentLoaded', () => {
 
+  if (whatsapp) {
     setInterval(() => {
-        whatsapp.classList.add("shake");
+      whatsapp.classList.add("shake");
 
-        setTimeout(() => {
-            whatsapp.classList.remove("shake");
-        }, 800);
-    }, 10000);
+      setTimeout(() => {
+        whatsapp.classList.remove("shake");
+      }, 800);
+    }, 3000);
+  }
 });
